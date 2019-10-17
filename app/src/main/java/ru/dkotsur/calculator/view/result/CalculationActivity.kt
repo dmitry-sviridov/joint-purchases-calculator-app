cd47772380aa714b4cba87d94cdeb1fb0f0e7055
dkotsur <Dmitrijkocur@gmail.com>
2019-10-17 19:02:58 +0300

    add result activity

---
+++ app/src/main/java/ru/dkotsur/calculator/view/result/CalculationActivity.kt
@@ -0,0 +1,13 @@
+package ru.dkotsur.calculator.view.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.dkotsur.calculator.R
+
+class CalculationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculation)
+    }
+}
